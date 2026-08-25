2b4acec7a1177b8f5d3aa695fe2e7c47f268a124
Alexi Dg <alexisdurangomez588@gmail.com>
2026-08-25 10:45:17 -0500

    feat: :elephant: configure PostgreSQL persistence

--- src/InventarioVentas.API/Program.cs
+++ src/InventarioVentas.API/Program.cs
@@ -1,12 +1,24 @@
+using InventarioVentas.API.Data.Configurations;
 using InventarioVentas.API.Modules.Categorias.Interfaces;
-using InventarioVentas.API.Modules.Categorias.Services; // o donde esté CategoriasService
-
+using InventarioVentas.API.Modules.Categorias.Services;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new InvalidOperationException(
+        "La configuración 'ConnectionStrings:DefaultConnection' es obligatoria para usar PostgreSQL.");
+}
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<CategoriaDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 builder.Services.AddScoped<ICategoriasService, CategoriasService>();
 builder.Services.AddControllers();
 
@@ -22,8 +34,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapControllers();
-
 app.MapControllers();
 
 app.Run();
