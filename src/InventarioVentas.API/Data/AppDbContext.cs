7a573f997913588952dfc82b7db45797dc5d910a
Alexi Dg <alexisdurangomez588@gmail.com>
2026-08-28 14:54:36 -0500

    feat: :bust_in_silhouette: add Identity user persistence

--- src/InventarioVentas.API/Data/AppDbContext.cs
+++ src/InventarioVentas.API/Data/AppDbContext.cs
@@ -4,9 +4,11 @@ using InventarioVentas.API.Modules.Customers.Models;
 using InventarioVentas.API.Modules.Categories.Models;
 using InventarioVentas.API.Modules.Products.Models;
 using InventarioVentas.API.Modules.Sales.Models;
+using InventarioVentas.API.Modules.auth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityUserContext<ApplicationUser, Guid>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
@@ -23,6 +25,13 @@ public class AppDbContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        modelBuilder.Entity<ApplicationUser>(entity =>
+        {
+            entity.Property(user => user.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+        });
     }
 
 
